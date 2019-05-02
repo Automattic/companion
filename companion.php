6659da96c0078cda9769cb080f81f74fd2c260bd
Brandon Kraft <public@brandonkraft.com>
2019-05-02 10:32:49 -0500

    Remove JP Constants, Added to Beta Plugin.

--- companion.php
+++ companion.php
@@ -3,7 +3,7 @@
 Plugin Name: Companion Plugin
 Plugin URI: https://github.com/Automattic/companion
 Description: Helps keep the launched WordPress in order.
-Version: 1.6
+Version: 1.7
 Author: Osk
 */
 
@@ -19,11 +19,6 @@ add_action( 'wp_login', 'companion_wp_login', 1, 2 );
 add_action( 'after_setup_theme', 'companion_after_setup_theme' );
 add_action( 'admin_notices', 'companion_admin_notices' );
 add_action( 'pre_current_active_plugins', 'companion_hide_plugin' );
-/*
- * Run this function as early as we can relying in WordPress loading plugin in alphabetical order
- */
-companion_tamper_with_jetpack_constants();
-add_action( 'init', 'companion_add_jetpack_constants_option_page' );
 
 function companion_admin_notices() {
 	if ( function_exists( 'get_current_screen' ) ) {
@@ -127,85 +122,4 @@ function companion_site_uses_https() {
 	$url = network_site_url();
 	$scheme = wp_parse_url( $url )['scheme'];
 	return 'https' === $scheme;
-}
-
-function companion_add_jetpack_constants_option_page() {
-	if ( ! companion_is_jetpack_here() ) {
-		return;
-	}
-	if ( ! class_exists( 'RationalOptionPages' ) ) {
-		require 'RationalOptionPages.php';
-	}
-
-	$jetpack_sandbox_domain = defined( 'JETPACK__SANDBOX_DOMAIN' ) ? JETPACK__SANDBOX_DOMAIN : '';
-	$deprecated = '<strong>' . sprintf(
-		esc_html__( 'This is no longer needed see %s.', 'companion' ),
-		'<code>JETPACK__SANDBOX_DOMAIN</code>'
-	) . '</strong>';
-
-	$options_page = array(
-		'companion' => array(
-			'page_title' => __( 'Jurassic Ninja Tweaks for Jetpack Constants', 'companion' ),
-			'menu_title' => __( 'Jetpack Constants', 'companion' ),
-			'menu_slug' => 'companion_settings',
-			'parent_slug' => 'options-general.php',
-			'sections' => array(
-				'jetpack_tweaks' => array(
-					'title' => __( 'Sites', 'companion' ),
-					'text' => '<p>' . __( 'Configure some defaults constants used by Jetpack.', 'companion' ) . '</p>',
-					'fields' => array(
-						'jetpack_sandbox_domain' => array(
-							'id' => 'jetpack_sandbox_domain',
-							'title' => __( 'JETPACK__SANDBOX_DOMAIN', 'companion' ),
-							'text' => sprintf(
-								esc_html__( "The domain of a WordPress.com Sandbox to which you wish to send all of Jetpack's remote requests. Must be a ___.wordpress.com subdomain with DNS permanently pointed to a WordPress.com sandbox. Current value for JETPACK__SANDBOX_DOMAIN: %s", 'companion' ),
-								'<code>' . esc_html( $jetpack_sandbox_domain ) . '</code>'
-							),
-							'placeholder' => esc_attr( $jetpack_sandbox_domain ),
-						),
-						'jetpack_beta_blocks' => array(
-							'id' => 'jetpack_beta_blocks',
-							'title' => __( 'JETPACK_BETA_BLOCKS', 'companion' ),
-							'text' =>
-								esc_html__( 'Check to enable Jetpack blocks for Gutenberg that are on Beta stage of development', 'companion' ),
-							'type' => 'checkbox',
-						),
-						'jetpack_protect_api_host' => array(
-							'id' => 'jetpack_protect_api_host',
-							'title' => __( 'JETPACK_PROTECT__API_HOST', 'companion' ),
-							'text' => sprintf(
-								esc_html__( "Base URL for API requests to Jetpack Protect's REST API. Current value for JETPACK_PROTECT__API_HOST: %s", 'companion' ),
-								'<code>' . esc_html( JETPACK_PROTECT__API_HOST ) . '</code>'
-							),
-							'placeholder' => esc_attr( JETPACK_PROTECT__API_HOST ),
-						),
-					),
-				),
-			),
-		),
-	);
-	new RationalOptionPages( $options_page );
-}
-
-function companion_is_jetpack_here() {
-	return class_exists( 'Jetpack' );
-}
-
-function companion_get_option( $slug, $default = null ) {
-	$options = get_option( 'companion', array() );
-	return isset( $options[ $slug ] )  ? $options[ $slug ] : $default;
-}
-
-
-function companion_tamper_with_jetpack_constants() {
-	if ( companion_get_option( 'jetpack_sandbox_domain', '' ) ) {
-		define( 'JETPACK__SANDBOX_DOMAIN', companion_get_option( 'jetpack_sandbox_domain', '' ) );
-	}
-
-	if ( companion_get_option( 'jetpack_protect_api_host', '' ) ) {
-		define( 'JETPACK_PROTECT__API_HOST', companion_get_option( 'jetpack_protect_api_host', '' ) );
-	}
-	if ( companion_get_option( 'jetpack_beta_blocks', '' ) ) {
-		define( 'JETPACK_BETA_BLOCKS', companion_get_option( 'jetpack_beta_blocks', '' ) ? true : false );
-	}
 }
