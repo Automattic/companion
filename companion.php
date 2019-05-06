1d852f32185fcdc0a55f7272afe97fcb09fcf529
oskosk <oskosk@gmail.com>
2019-05-06 11:44:12 -0300

    Bump version to 1.8

--- companion.php
+++ companion.php
@@ -3,7 +3,7 @@
 Plugin Name: Companion Plugin
 Plugin URI: https://github.com/Automattic/companion
 Description: Helps keep the launched WordPress in order.
-Version: 1.7
+Version: 1.8
 Author: Osk
 */
 
