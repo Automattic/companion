401899eea28e6d3fc3d53f73856cee2036dc38b9
oskosk <oskosk@gmail.com>
2021-12-10 09:01:32 -0300

    Fix the way JETPACK_PROTECT__API_HOST is handled in Jetpack constants

--- companion.php
+++ companion.php
@@ -247,6 +247,7 @@ function companion_add_jetpack_constants_option_page() {
 	);
 
 	$jetpack_sandbox_domain = defined( 'JETPACK__SANDBOX_DOMAIN' ) ? JETPACK__SANDBOX_DOMAIN : '';
+	$jetpack_protect_api_host = defined( 'JETPACK_PROTECT__API_HOST' ) ? JETPACK_PROTECT__API_HOST : '';
 
 	$global_fields = array(
 		'jetpack_sandbox_domain' => array(
@@ -275,9 +276,9 @@ function companion_add_jetpack_constants_option_page() {
 				'title' => __( 'JETPACK_PROTECT__API_HOST', 'companion' ),
 				'text' => sprintf(
 					esc_html__( "Base URL for API requests to Jetpack Protect's REST API. Current value for JETPACK_PROTECT__API_HOST: %s", 'companion' ),
-					'<code>' . esc_html( JETPACK_PROTECT__API_HOST ) . '</code>'
+					'<code>' . esc_html( $jetpack_protect_api_host ) . '</code>'
 				),
-				'placeholder' => esc_attr( JETPACK_PROTECT__API_HOST ),
+				'placeholder' => esc_attr( $jetpack_protect_api_host ),
 			),
 			'jetpack_should_not_use_connection_iframe' => array(
 				'id' => 'jetpack_should_not_use_connection_iframe',
