acf2469983b8d5be257ebc830fc1c53ce12869ef
Leo Germani <leogermani@automattic.com>
2022-05-03 14:14:16 -0300

    bump plugin version

--- companion.php
+++ companion.php
@@ -3,7 +3,7 @@
 Plugin Name: Companion Plugin
 Plugin URI: https://github.com/Automattic/companion
 Description: Helps keep the launched WordPress in order.
-Version: 1.24
+Version: 1.25
 Author: Osk
 */
 
