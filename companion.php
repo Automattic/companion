684e353460119118f45a676638b7c26f54934833
Brandon Kraft <public@brandonkraft.com>
2024-02-14 14:03:15 -0600

    Remove reference to sudo http enable

--- companion.php
+++ companion.php
@@ -324,8 +324,7 @@ function companion_add_jetpack_constants_option_page() {
 			'title' => __( 'JETPACK__SANDBOX_DOMAIN', 'companion' ),
 			'text' => sprintf(
 				esc_html__( "The domain of a WordPress.com Sandbox to which you wish to send all of Jetpack's remote requests. Current value for JETPACK__SANDBOX_DOMAIN: %s %s", 'companion' ),
-				'<code>' . esc_html( $jetpack_sandbox_domain ) . '</code>',
-				'<br><strong>' . __( 'Remember to enable global http in your sandbox', 'companion' ) . '</strong>'
+				'<code>' . esc_html( $jetpack_sandbox_domain ) . '</code>'
 			),
 			'placeholder' => esc_attr( $jetpack_sandbox_domain ),
 		),
